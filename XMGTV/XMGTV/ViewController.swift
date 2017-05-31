2905cc015350c9e4322607ade24fdaf30872ad7d
caiiiac <caiiiac@163.com>
2017-05-31 20:30:37 +0800

    project init

---
+++ XMGTV/XMGTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/5/31.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
