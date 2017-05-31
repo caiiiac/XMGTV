9e75b0d5ddcd4735632cbb64635365cf6ae8b2b7
caiiiac <caiiiac@163.com>
2017-05-31 21:54:34 +0800

    基础架构

--- XMGTV/XMGTV/Classes/Home/HomeViewController.swift
+++ XMGTV/XMGTV/Classes/Home/HomeViewController.swift
@@ -14,22 +14,45 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        setupUI()
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+// MARK:- UI
+extension HomeViewController {
+    fileprivate func setupUI() {
+        setupNavigationBar()
     }
     
+    fileprivate func setupNavigationBar() {
+        // 左侧logoItem
+        let logoImage = UIImage(named: "home-logo")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: logoImage, style: .plain, target: nil, action: nil)
+        
+        // 设置右侧收藏的item
+        let collectImage = UIImage(named: "search_btn_follow")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: collectImage, style: .plain, target: self, action: #selector(followItemClick))
+        
+        
+        // 搜索框
+        let searchFrame = CGRect(x: 0, y: 0, width: 200, height: 32)
+        let searchBar = UISearchBar(frame: searchFrame)
+        searchBar.placeholder = "主播昵称/房间号/链接"
+        navigationItem.titleView = searchBar
+        searchBar.searchBarStyle = .minimal
+        let searchFiled = searchBar.value(forKey: "_searchField") as? UITextField
+        searchFiled?.textColor = UIColor.white
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+}
 
+// MARK:- 事件监听函数
+extension HomeViewController {
+    @objc fileprivate func followItemClick() {
+        print("------")
+    }
 }
