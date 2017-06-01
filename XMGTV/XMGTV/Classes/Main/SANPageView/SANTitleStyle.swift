4ba932532e8ad4a7b739c8745f5453e22f62b279
caiiiac <caiiiac@163.com>
2017-06-01 21:35:21 +0800

    add pageView

---
+++ XMGTV/XMGTV/Classes/Main/SANPageView/SANTitleStyle.swift
@@ -0,0 +1,13 @@
+//
+//  SANTitleStyle.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/6/1.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class SANTitleStyle: NSObject {
+
+}
