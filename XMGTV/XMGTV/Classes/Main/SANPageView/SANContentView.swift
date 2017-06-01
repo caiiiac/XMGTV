17cfd4435e58b3e59a8a76fb369d87a272aaa89e
caiiiac <caiiiac@163.com>
2017-06-01 22:22:08 +0800

    补充contentView

--- XMGTV/XMGTV/Classes/Main/SANPageView/SANContentView.swift
+++ XMGTV/XMGTV/Classes/Main/SANPageView/SANContentView.swift
@@ -8,20 +8,80 @@
 
 import UIKit
 
+private let kContentCellID = "kContentCellID"
+
 class SANContentView: UIView {
 
     fileprivate var childVcs : [UIViewController]
     fileprivate var parentVc : UIViewController
+    
+    fileprivate lazy var collectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = self.bounds.size
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: self.bounds, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kContentCellID)
+        collectionView.isPagingEnabled = true
+        collectionView.bounces = false
+        collectionView.scrollsToTop = false
+         collectionView.showsHorizontalScrollIndicator = false
+        
+        collectionView.dataSource = self
+        
+        return collectionView
+    }()
     
     init(frame: CGRect, childVcs : [UIViewController], parentVc : UIViewController) {
         self.childVcs = childVcs
         self.parentVc = parentVc
         
         super.init(frame: frame)
+        
+        
+        setupUI()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+}
+
+//MARK: - UI
+extension SANContentView {
+    fileprivate func setupUI() {
+        
+        //将子控制器添加到父控制器中
+        for childVc in childVcs {
+            parentVc.addChildViewController(childVc)
+        }
+       
+        addSubview(collectionView)
+    }
+}
+
+//MARK: - UICollectionViewDataSource
+extension SANContentView : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return childVcs.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kContentCellID, for: indexPath)
+        
+        //先将cell子控件清空,防止多次add
+        for subView in cell.contentView.subviews {
+            subView.removeFromSuperview()
+        }
+        
+        let childVc = childVcs[indexPath.item]
+        childVc.view.frame = cell.contentView.bounds
+        cell.contentView.addSubview(childVc.view)
+        
+        return cell
+        
+    }
 }
