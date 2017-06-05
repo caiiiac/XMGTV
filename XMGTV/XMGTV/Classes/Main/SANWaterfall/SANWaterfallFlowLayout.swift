a1a53f7b5f086f3332005b11e4253e6b5aee5f6a
caiiiac <caiiiac@163.com>
2017-06-05 23:11:01 +0800

    add 自定义瀑布流

---
+++ XMGTV/XMGTV/Classes/Main/SANWaterfall/SANWaterfallFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  SANWaterfallFlowLayout.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/6/5.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class SANWaterfallFlowLayout: UICollectionViewFlowLayout {
+
+}
