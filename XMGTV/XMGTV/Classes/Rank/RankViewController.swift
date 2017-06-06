3feac2df8fc692bdd59432ebe51f153c062a0395
caiiiac <caiiiac@163.com>
2017-06-06 23:12:42 +0800

    瀑布流测试代码

--- XMGTV/XMGTV/Classes/Rank/RankViewController.swift
+++ XMGTV/XMGTV/Classes/Rank/RankViewController.swift
@@ -8,28 +8,51 @@
 
 import UIKit
 
+private let ReuseIdentifier = "RankCollectionViewCell"
+
 class RankViewController: UIViewController {
+    
+    fileprivate lazy var collectionView : UICollectionView = {
+       let layout = SANWaterfallFlowLayout()
+        layout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+        layout.minimumLineSpacing = 10
+        layout.minimumLineSpacing = 10
+        
+        layout.dataSource = self
+        
+        let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.addSubview(collectionView)
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: ReuseIdentifier)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension RankViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
     }
-    */
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReuseIdentifier, for: indexPath)
+        cell.contentView.backgroundColor = UIColor.randomColor()
+        
+        return cell
+    }
+}
 
+extension RankViewController : SANWaterfallFlowLayoutDataSource {
+    func numberOfCols(_ waterfall: SANWaterfallFlowLayout) -> Int {
+        return 3
+    }
+    func waterfall(_ waterfall: SANWaterfallFlowLayout, item: Int) -> CGFloat {
+        return CGFloat(arc4random_uniform(100) + 100)
+    }
 }
