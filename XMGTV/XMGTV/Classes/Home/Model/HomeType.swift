399704a55582b3e27e8379e771a945bec31cb977
caiiiac <caiiiac@163.com>
2017-06-07 23:37:51 +0800

    add model

---
+++ XMGTV/XMGTV/Classes/Home/Model/HomeType.swift
@@ -0,0 +1,16 @@
+//
+//  HomeType.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/6/7.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class HomeType: BaseModel {
+
+    var title : String = ""
+    var type : Int = 0
+    
+}
