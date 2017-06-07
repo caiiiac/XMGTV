0d0ddd588f750e8f536a19033fe05989eba322b7
caiiiac <caiiiac@163.com>
2017-06-07 23:37:03 +0800

    add cell

---
+++ XMGTV/XMGTV/Classes/Home/Cell/FocusViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FocusViewCell.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/6/7.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class FocusViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
