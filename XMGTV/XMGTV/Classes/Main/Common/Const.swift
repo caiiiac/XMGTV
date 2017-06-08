542e53e7404f2249543fe88e8a8efb6a56f6a274
caiiiac <caiiiac@163.com>
2017-06-08 21:44:47 +0800

    add const

---
+++ XMGTV/XMGTV/Classes/Main/Common/Const.swift
@@ -0,0 +1,15 @@
+//
+//  Const.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/6/8.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
+
+let kNavigationBarH : CGFloat = 44
+let kStatusBarH : CGFloat = 20
