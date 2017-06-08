c309a06878118064499376e820498e0e7851d11d
caiiiac <caiiiac@163.com>
2017-06-08 21:46:45 +0800

    注释掉

--- XMGTV/XMGTV/Classes/Home/Model/AnchorModel.swift
+++ XMGTV/XMGTV/Classes/Home/Model/AnchorModel.swift
@@ -21,30 +21,30 @@ class AnchorModel: BaseModel {
     var isEvenIndex : Bool = false
 }
 
-
-extension AnchorModel {
-    func inserIntoDB() {
-        // 1.拼接插入语句
-        let insertSQL = "INSERT INTO t_focus (roomid, name, pic51, pic74, live) VALUES (\(roomid), '\(name)', '\(pic51)', '\(pic74)', \(live));"
-        
-        // 2.执行语句
-        if SqliteTools.execSQL(insertSQL) {
-            print("插入成功")
-        } else {
-            print("插入失败")
-        }
-    }
-    
-    func deleteFromDB() {
-        // 1.拼接删除语句
-        let deleteSQL = "DELETE FROM t_focus WHERE roomid = \(roomid);"
-        
-        // 2.执行语句
-        if SqliteTools.execSQL(deleteSQL) {
-            print("删除成功")
-        } else {
-            print("删除失败")
-        }
-    }
-
-}
+//
+//extension AnchorModel {
+//    func inserIntoDB() {
+//        // 1.拼接插入语句
+//        let insertSQL = "INSERT INTO t_focus (roomid, name, pic51, pic74, live) VALUES (\(roomid), '\(name)', '\(pic51)', '\(pic74)', \(live));"
+//        
+//        // 2.执行语句
+//        if SqliteTools.execSQL(insertSQL) {
+//            print("插入成功")
+//        } else {
+//            print("插入失败")
+//        }
+//    }
+//    
+//    func deleteFromDB() {
+//        // 1.拼接删除语句
+//        let deleteSQL = "DELETE FROM t_focus WHERE roomid = \(roomid);"
+//        
+//        // 2.执行语句
+//        if SqliteTools.execSQL(deleteSQL) {
+//            print("删除成功")
+//        } else {
+//            print("删除失败")
+//        }
+//    }
+//
+//}
