ee70498a5fd88c34e6044716c45efa8dfc261a8a
caiiiac <caiiiac@163.com>
2017-06-09 15:50:53 +0800

    实现全屏pop

--- XMGTV/XMGTV/Classes/Main/Controller/SANNavigationController.swift
+++ XMGTV/XMGTV/Classes/Main/Controller/SANNavigationController.swift
@@ -13,11 +13,38 @@ class SANNavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 使用运行时,打印手势中所有属性
+//        var count : UInt32 = 0
+//        let ivars = class_copyIvarList(UIGestureRecognizer.self, &count)!
+//        for i in 0..<count {
+//            let nameP = ivar_getName(ivars[Int(i)])!
+//            let name = String(cString: nameP)
+//            print(name)
+//        }
+        
+        guard let targets = interactivePopGestureRecognizer!.value(forKey: "_targets") as? [NSObject] else {
+            return
+        }
+        
+        let targetObjc = targets[0]
+        print(targetObjc)
+        
+        let target = targetObjc.value(forKey: "target")
+        let action = Selector(("handleNavigationTransition:"))
+        
+        let panGes = UIPanGestureRecognizer(target: target, action: action)
+        
+        view.addGestureRecognizer(panGes)
+        
+        
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
 
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        viewController.hidesBottomBarWhenPushed = true
+        super.pushViewController(viewController, animated: animated)
+    }
 }
