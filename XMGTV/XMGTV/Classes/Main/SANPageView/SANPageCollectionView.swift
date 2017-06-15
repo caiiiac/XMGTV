be38015c98a1531c78161a6770d327f20f642d31
caiiiac <caiiiac@163.com>
2017-06-15 17:48:20 +0800

    SANPageCollectionView 扩充

--- XMGTV/XMGTV/Classes/Main/SANPageView/SANPageCollectionView.swift
+++ XMGTV/XMGTV/Classes/Main/SANPageView/SANPageCollectionView.swift
@@ -14,15 +14,23 @@ protocol SANPageCollectionViewDataSource : class {
     func pageCollectionView(_ pageCollectionView : SANPageCollectionView ,_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
 }
 
+protocol SANPageCollectionViewDelegate : class {
+    func pageCollectionView(_ pageCollectionView: SANPageCollectionView, didSelectItemAt indexPath: IndexPath)
+}
+
 class SANPageCollectionView: UIView {
     
     weak var dataSource : SANPageCollectionViewDataSource?
+    weak var delegate : SANPageCollectionViewDelegate?
     
     fileprivate var titles : [String]
     fileprivate var isTitleInTop : Bool
     fileprivate var style : SANTitleStyle
-    fileprivate var layout : UICollectionViewFlowLayout
+    fileprivate var layout : SANPageCollectionViewLayout
     fileprivate var collectionView : UICollectionView!
+    fileprivate var pageControl : UIPageControl!
+    fileprivate var titleView : SANTitleView!
+    fileprivate var sourceIndexPath : IndexPath = IndexPath(item: 0, section: 0)
     
     init(frame: CGRect, titles : [String], style : SANTitleStyle, isTitleInTop : Bool, layout : SANPageCollectionViewLayout) {
         self.titles = titles
@@ -47,28 +55,29 @@ extension SANPageCollectionView {
         // 1.创建titleView
         let titleY = isTitleInTop ? 0 : bounds.height - style.titleHeight
         let titleFrame = CGRect(x: 0, y: titleY, width: bounds.width, height: style.titleHeight)
-        let titleView = SANTitleView(frame: titleFrame, titles: titles, style: style)
+        titleView = SANTitleView(frame: titleFrame, titles: titles, style: style)
         addSubview(titleView)
-        titleView.backgroundColor = UIColor.randomColor()
+        titleView.delegate = self
         
         // 2.创建UIPageControl
         let pageControlHeight : CGFloat = 20
         let pageControlY = isTitleInTop ? (bounds.height - pageControlHeight) : (bounds.height - pageControlHeight - style.titleHeight)
         let pageControlFrame = CGRect(x: 0, y: pageControlY, width: bounds.width, height: pageControlHeight)
-        let pageControl = UIPageControl(frame: pageControlFrame)
+        pageControl = UIPageControl(frame: pageControlFrame)
         pageControl.numberOfPages = 4
+        pageControl.isEnabled = false
         addSubview(pageControl)
-        pageControl.backgroundColor = UIColor.randomColor()
         
         // 3.创建UICollectionView
         let collectionViewY = isTitleInTop ? style.titleHeight : 0
         let collectionViewFrame = CGRect(x: 0, y: collectionViewY, width: bounds.width, height: bounds.height - style.titleHeight - pageControlHeight)
         collectionView = UICollectionView(frame: collectionViewFrame, collectionViewLayout: layout)
         collectionView.dataSource = self
+        collectionView.delegate = self
         collectionView.isPagingEnabled = true
         collectionView.showsHorizontalScrollIndicator = false
         addSubview(collectionView)
-        collectionView.backgroundColor = UIColor.randomColor()
+        pageControl.backgroundColor = collectionView.backgroundColor
     }
 }
 
@@ -92,10 +101,69 @@ extension SANPageCollectionView : UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dataSource?.pageCollectionView(self, numberOfItemsInSection: section) ?? 0
+        let itemCount = dataSource?.pageCollectionView(self, numberOfItemsInSection: section) ?? 0
+        
+        if section == 0 {
+            pageControl.numberOfPages = (itemCount - 1) / (layout.cols * layout.rows) + 1
+        }
+        
+        return itemCount
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         return dataSource!.pageCollectionView(self, collectionView, cellForItemAt: indexPath)
+    }
+}
+
+
+// MARK:- UICollectionViewDelegate
+extension SANPageCollectionView : UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        delegate?.pageCollectionView(self, didSelectItemAt: indexPath)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        scrollViewEndScroll()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            scrollViewEndScroll()
+        }
+    }
+    
+    fileprivate func scrollViewEndScroll() {
+        // 1.取出在屏幕中显示的Cell
+        let point = CGPoint(x: layout.sectionInset.left + 1 + collectionView.contentOffset.x, y: layout.sectionInset.top + 1)
+        guard let indexPath = collectionView.indexPathForItem(at: point) else { return }
+        
+        // 2.判断分组是否有发生改变
+        if sourceIndexPath.section != indexPath.section {
+            // 3.1.修改pageControl的个数
+            let itemCount = dataSource?.pageCollectionView(self, numberOfItemsInSection: indexPath.section) ?? 0
+            pageControl.numberOfPages = (itemCount - 1) / (layout.cols * layout.rows) + 1
+            
+            // 3.2.设置titleView位置
+            titleView.setTitleWithProgress(1.0, sourceIndex: sourceIndexPath.section, targetIndex: indexPath.section)
+            
+            // 3.3.记录最新indexPath
+            sourceIndexPath = indexPath
+        }
+        
+        // 3.根据indexPath设置pageControl
+        pageControl.currentPage = indexPath.item / (layout.cols * layout.rows)
+    }
+}
+
+
+// MARK:- SANTitleViewDelegate
+extension SANPageCollectionView : SANTitleViewDelegate {
+    func titleView(_ titleView: SANTitleView, selectedIndex index: Int) {
+        let indexPath = IndexPath(item: 0, section: index)
+        collectionView.scrollToItem(at: indexPath, at: .left, animated: false)
+        collectionView.contentOffset.x -= layout.sectionInset.left
+        
+        scrollViewEndScroll()
     }
 }
