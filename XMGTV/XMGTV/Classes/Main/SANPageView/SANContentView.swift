9592ee5409b37d630af32e7fce9c04da9cad52cc
caiiiac <caiiiac@163.com>
2017-06-22 22:44:30 +0800

    修复滚动BUG

--- XMGTV/XMGTV/Classes/Main/SANPageView/SANContentView.swift
+++ XMGTV/XMGTV/Classes/Main/SANPageView/SANContentView.swift
@@ -163,11 +163,14 @@ extension SANContentView : UICollectionViewDelegate {
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         delegate?.contentViewEndScroll?(self)
+        scrollView.isScrollEnabled = true
     }
     
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         if !decelerate {
             delegate?.contentViewEndScroll?(self)
+        } else {
+            scrollView.isScrollEnabled = false
         }
     }
 }
