91b98989f3ea973286e95fcbb4cebfd786153ddf
caiiiac <caiiiac@163.com>
2017-06-25 23:06:56 +0800

    add AttrStringGenerator

---
+++ XMGTV/XMGTV/Classes/Tools/AttrStringGenerator.swift
@@ -0,0 +1,13 @@
+//
+//  AttrStringGenerator.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/6/25.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class AttrStringGenerator: NSObject {
+
+}
