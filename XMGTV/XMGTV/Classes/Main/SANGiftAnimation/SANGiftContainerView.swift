f4a0912a7db2dd6dd296c0fd02e53d6363cc0cc1
caiiiac <caiiiac@163.com>
2017-06-27 23:13:24 +0800

    add SANGiftContainerView

---
+++ XMGTV/XMGTV/Classes/Main/SANGiftAnimation/SANGiftContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  SANGiftContainerView.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/6/27.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class SANGiftContainerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
