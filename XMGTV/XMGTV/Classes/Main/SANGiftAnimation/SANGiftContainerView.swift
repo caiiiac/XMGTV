2979197c32ac0eec39aa12db42248acd91e653d8
caiiiac <caiiiac@163.com>
2017-06-28 23:34:28 +0800

    SANGiftContainerView

--- XMGTV/XMGTV/Classes/Main/SANGiftAnimation/SANGiftContainerView.swift
+++ XMGTV/XMGTV/Classes/Main/SANGiftAnimation/SANGiftContainerView.swift
@@ -8,14 +8,81 @@
 
 import UIKit
 
+private let kChannelCount = 2
+private let kChannelViewH : CGFloat = 40
+private let kChannelMargin : CGFloat = 10
+
 class SANGiftContainerView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: 定义属性
+    fileprivate lazy var channelViews : [SANGiftChannelView] = [SANGiftChannelView]()
+    fileprivate lazy var cacheGiftModels : [SANGiftModel] = [SANGiftModel]()
+    
+    // MARK: 构造函数
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
+
+// MARK:- 设置UI界面
+extension SANGiftContainerView {
+    fileprivate func setupUI() {
+        // 1.根据当前的渠道数，创建SANGiftChannelView
+        let w : CGFloat = frame.width
+        let h : CGFloat = kChannelViewH
+        let x : CGFloat = 0
+        for i in 0..<kChannelCount {
+            let y : CGFloat = (h + kChannelMargin) * CGFloat(i)
+            
+            let channelView = SANGiftChannelView.loadFromNib()
+            channelView.frame = CGRect(x: x, y: y, width: w, height: h)
+            channelView.alpha = 0.0
+            addSubview(channelView)
+            channelViews.append(channelView)
+        }
+    }
+}
+
+
+extension SANGiftContainerView {
+    func showGiftModel(_ giftModel : SANGiftModel) {
+        // 1.判断正在忙的ChanelView和赠送的新礼物的(username/giftname)
+        if let channelView = checkUsingChanelView(giftModel) {
+            channelView.addOnceToCache()
+        }
+        
+        // 2.判断有没有闲置的ChanelView
+        if let channelView = checkIdleChannelView() {
+            channelView.giftModel = giftModel
+        }
+        
+        // 3.将数据放入缓存中
+        cacheGiftModels.append(giftModel)
+    }
+    
+    private func checkUsingChanelView(_ giftModel : SANGiftModel) -> SANGiftChannelView? {
+        for channelView in channelViews {
+            if giftModel.isEqual(channelView.giftModel) && channelView.state != .endAnimating {
+                return channelView
+            }
+        }
+        
+        return nil
+    }
+    
+    private func checkIdleChannelView() -> SANGiftChannelView? {
+        for channelView in channelViews {
+            if channelView.state == .idle {
+                return channelView
+            }
+        }
+        
+        return nil
+    }
+}
+ 
