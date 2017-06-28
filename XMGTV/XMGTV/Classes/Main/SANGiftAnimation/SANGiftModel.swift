a0e1148cc46a3c88fd3a1c27a35efa6b34f934d7
caiiiac <caiiiac@163.com>
2017-06-28 23:26:13 +0800

    add GiftModel

---
+++ XMGTV/XMGTV/Classes/Main/SANGiftAnimation/SANGiftModel.swift
@@ -0,0 +1,13 @@
+//
+//  SANGiftModel.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/6/28.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class SANGiftModel: NSObject {
+
+}
