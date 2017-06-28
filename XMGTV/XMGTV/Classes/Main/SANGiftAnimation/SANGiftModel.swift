d6cf67b11dd4fac9cd0578f015ae680878a0c12b
caiiiac <caiiiac@163.com>
2017-06-28 23:27:21 +0800

    SANGiftModel

--- XMGTV/XMGTV/Classes/Main/SANGiftAnimation/SANGiftModel.swift
+++ XMGTV/XMGTV/Classes/Main/SANGiftAnimation/SANGiftModel.swift
@@ -9,5 +9,28 @@
 import UIKit
 
 class SANGiftModel: NSObject {
+    var senderName : String = ""
+    var senderURL : String = ""
+    var giftName : String = ""
+    var giftURL : String = ""
+    
+    init(senderName : String, senderURL : String, giftName : String, giftURL : String) {
+        self.senderName = senderName
+        self.senderURL = senderURL
+        self.giftName = giftName
+        self.giftURL = giftURL
+    }
+    
+    override func isEqual(_ object: Any?) -> Bool {
+        guard let object = object as? SANGiftModel else {
+            return false
+        }
+        
+        guard object.giftName == giftName && object.senderName == senderName else {
+            return false
+        }
+        
+        return true
+    }
 
 }
