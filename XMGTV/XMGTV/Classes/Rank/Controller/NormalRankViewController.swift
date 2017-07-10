c4030c6d25769c296b38b59d0a05fec2c778ab6f
caiiiac <caiiiac@163.com>
2017-07-10 21:41:11 +0800

    add NormalRankViewController.swift

---
+++ XMGTV/XMGTV/Classes/Rank/Controller/NormalRankViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NormalRankViewController.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/7/10.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class NormalRankViewController: SubrankViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupSubrankUI(["日榜", "周榜", "月榜", "总榜"])
+    }
+ 
+
+}
