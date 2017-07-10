2ead5b15b0969c6c76e1701ee33c0930831a821c
caiiiac <caiiiac@163.com>
2017-07-10 21:41:26 +0800

    add WeekrankViewController.swift

---
+++ XMGTV/XMGTV/Classes/Rank/Controller/WeekrankViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeekrankViewController.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/7/10.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class WeekrankViewController: SubrankViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupSubrankUI(["上周", "本周"])
+    }
+ 
+}
