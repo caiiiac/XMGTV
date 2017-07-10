9be00147cb7cd9000f6992e4be6866f36de8e32b
caiiiac <caiiiac@163.com>
2017-07-10 21:40:40 +0800

    add RankType.swift

---
+++ XMGTV/XMGTV/Classes/Rank/Model/RankType.swift
@@ -0,0 +1,19 @@
+//
+//  RankType.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/7/10.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class RankType {
+    var typeName : String = ""
+    var typeNum : Int = 0
+    
+    init(typeName : String, typeNum : Int) {
+        self.typeName = typeName
+        self.typeNum = typeNum
+    }
+}
