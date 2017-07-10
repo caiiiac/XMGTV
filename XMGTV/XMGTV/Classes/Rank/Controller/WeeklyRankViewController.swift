a8d71b679c853ef6fd97a153aa046d3521bbddc8
caiiiac <caiiiac@163.com>
2017-07-10 21:41:18 +0800

    add WeeklyRankViewController.swift

---
+++ XMGTV/XMGTV/Classes/Rank/Controller/WeeklyRankViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WeeklyRankViewController.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/7/10.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class WeeklyRankViewController: DetailRankViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override init(rankType: RankType) {
+        super.init(rankType: rankType)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
