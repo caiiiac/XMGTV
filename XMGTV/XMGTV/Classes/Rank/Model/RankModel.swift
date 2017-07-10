f7c89669525ff2fb6fe78ab28b1f0568900d9cfe
caiiiac <caiiiac@163.com>
2017-07-10 21:55:39 +0800

    add 新VC

---
+++ XMGTV/XMGTV/Classes/Rank/Model/RankModel.swift
@@ -0,0 +1,13 @@
+//
+//  RankModel.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/7/10.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class RankModel: BaseModel {
+
+}
---
+++ XMGTV/XMGTV/Classes/Rank/ViewModel/DetailRankViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DetailRankViewModel.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/7/10.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class DetailRankViewModel: NSObject {
+
+}
