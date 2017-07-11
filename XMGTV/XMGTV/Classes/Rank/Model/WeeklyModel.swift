2c5d3ab1bdd0f2c5f478698768248342303378c8
caiiiac <caiiiac@163.com>
2017-07-11 21:26:29 +0800

    add WeeklyModel.swift

---
+++ XMGTV/XMGTV/Classes/Rank/Model/WeeklyModel.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyModel.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/7/11.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class WeeklyModel: BaseModel {
+    var giftName : String = ""
+    var giftNum : Int = 0
+    var nickname : String = ""
+    var giftAppImg : String = ""
+}
+
