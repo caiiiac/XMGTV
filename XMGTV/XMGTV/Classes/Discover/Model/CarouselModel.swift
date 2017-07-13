341dc7406306fb6be517a8c37c2dd98684a9b7c8
caiiiac <caiiiac@163.com>
2017-07-13 10:35:38 +0800

    add CarouselModel.swift

---
+++ XMGTV/XMGTV/Classes/Discover/Model/CarouselModel.swift
@@ -0,0 +1,15 @@
+//
+//  CarouselModel.swift
+//  XMGTV
+//
+//  Created by 唐三彩 on 2017/7/13.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class CarouselModel: BaseModel {
+    var name : String = ""
+    var picUrl : String = ""
+    var linkUrl : String = ""
+}
